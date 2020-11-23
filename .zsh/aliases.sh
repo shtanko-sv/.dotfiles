0ae0ed8c3bcfc84cb854c93e2d84e8171041cceb
Sergey Shtanko <shtanko.sergey@gmail.com>
2020-11-23 13:49:28 +0300

    Add pip_upgrade_packages alias
    This is needed for easy updates pip
    packages. Was copy/pasted from:
    https://stackoverflow.com/questions/2720014/how-to-upgrade-all-python-packages-with-pip

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -1,1 +1,2 @@
 alias dot='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias pip_upgrade_packages="pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
